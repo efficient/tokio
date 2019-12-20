5cb607cf2510327e8b8d419f97dab2595ef99183
Sol Boucher <sboucher@cmu.edu>
2019-12-20 14:31:54 -0500

    figures/si_us.rs: Fix handling of times in s
    Turns out they have an extra space at the end in the wrk output...

--- figures/si_us.rs
+++ figures/si_us.rs
@@ -4,10 +4,11 @@
 extern "C" fn transform_y(y: &str) -> f64 {
 	let oom = y.find(char::is_alphabetic).unwrap();
 	let val: f64 = y[..oom].parse().unwrap();
-	match &y[oom..] {
+	let oom = &y[oom..];
+	match oom {
 	"us" => val,
 	"ms" => val * 1_000.0,
-	"s" => val * 1_000_000.0,
-	_ => panic!("unrecognized si suffix"),
+	"s " => val * 1_000_000.0,
+	_ => panic!(format!("unrecognized si suffix: '{}'", oom)),
 	}
 }
